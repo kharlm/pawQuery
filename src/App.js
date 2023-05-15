c9d591dd007df6adc612615653a25fe6c2272e6b
kharlm <kharlmccatty@yahoo.com>
2023-05-15 19:04:04 -0400

    created love language article

--- src/App.js
+++ src/App.js
@@ -5,92 +5,85 @@ function App() {
   return (
     <PageContainer>
       <ArticleContainer>
-        <Title>How to Clean a Dog's Ears</Title>
+        <Title>
+          Understanding the Five Love Languages: A Path to Deeper Connections
+        </Title>
         <ImageContainer>
           <Image
             src={
-              "https://images.unsplash.com/photo-1583511666372-62fc211f8377?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8ZG9nJTIwZWFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+              "https://images.unsplash.com/photo-1516589178581-6cd7833ae3b2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bG92ZSUyMGxhbmd1YWdlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
             }
-            alt="Dog ear"
+            alt="Hand heart"
           />
-          {/* <Image
-            src={
-              "https://plus.unsplash.com/premium_photo-1677234146706-9593d685317c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGRvZyUyMGNsZWFuaW5nJTIwc3VwcGxpZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
-            }
-            alt="Supplies"
-          />
-          <Image
-            src={
-              "https://images.unsplash.com/photo-1632236705239-ac6f4908638d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGNsZWFuaW5nJTIwZG9nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-            }
-            alt="Cleaning"
-          /> */}
         </ImageContainer>
         <Paragraph>
-          If you are a dog owner, you know that ear infections are a common
-          problem for dogs. Keeping your dog's ears clean is an important part
-          of their overall health and well-being. However, cleaning a dog's ears
-          can be tricky and should be done carefully to avoid hurting your furry
-          friend. In this article, we will guide you on how to clean a dog's
-          ears effectively and safely.
+          In the realm of relationships, understanding how to express and
+          receive love is paramount. Just as languages vary across cultures, so
+          too do the ways in which individuals prefer to give and receive love.
+          Developed by Dr. Gary Chapman, the concept of the five love languages
+          offers valuable insights into our unique emotional needs and
+          communication styles. In this article, we'll explore the five love
+          languages and delve into how they can enhance our relationships.
         </Paragraph>
-        <SubTitle>1. Gather Supplies</SubTitle>
+        <SubTitle>1. Words of Affirmation</SubTitle>
         <Paragraph>
-          Before you start, make sure you have all the necessary supplies. You
-          will need cotton balls or gauze pads, a cleaning solution specifically
-          made for dogs, and some treats to reward your dog for being
-          cooperative during the cleaning process.
+          For individuals whose primary love language is words of affirmation,
+          verbal expressions of love and appreciation carry great significance.
+          Simple yet powerful words like "I love you," "You're amazing," or "I
+          appreciate you" can deeply resonate with them. Compliments,
+          encouragement, and kind words hold the key to their hearts,
+          reinforcing their sense of value and building emotional connections.
         </Paragraph>
-        <SubTitle>2. Get Your Dog Ready</SubTitle>
+        <SubTitle>2. Acts of Service</SubTitle>
         <Paragraph>
-          Choose a comfortable and calm location for your dog and make sure they
-          are relaxed before starting. You can also have a second person to help
-          you hold your dog still if needed.
+          Some people feel most loved when their partner performs acts of
+          service for them. These acts can be as simple as making their favorite
+          meal, doing household chores, or running errands on their behalf.
+          Actions speak louder than words for those who prioritize acts of
+          service as their love language, as these gestures demonstrate care,
+          thoughtfulness, and a willingness to support and help them.
         </Paragraph>
-        <SubTitle>3. Examine the Ears</SubTitle>
+        <SubTitle>3. Receiving Gifts</SubTitle>
         <Paragraph>
-          Start by examining your dog's ears to check if there are any signs of
-          redness, discharge, or inflammation. If you notice any of these signs
-          or if your dog has been scratching their ears excessively, it's best
-          to take them to the vet for a check-up before cleaning their ears.
+          Gifts are tangible symbols of love for individuals whose primary love
+          language is receiving gifts. It's not necessarily about materialism or
+          extravagance, but rather the thought and effort put into selecting a
+          meaningful gift. The gesture itself, regardless of the monetary value,
+          demonstrates attention, consideration, and a deep understanding of
+          their desires and preferences.
         </Paragraph>
-        <SubTitle>4. Apply the Cleaning Solution</SubTitle>
+        <SubTitle>4. Quality Time</SubTitle>
         <Paragraph>
-          Next, apply the cleaning solution to a cotton ball or gauze pad. Be
-          careful not to use a cotton swab, as this can push debris further into
-          the ear canal and cause injury. Gently lift your dog's ear flap and
-          wipe the inside of the ear with the cleaning solution.
+          In our busy lives, undivided attention is a precious gift. Those who
+          prioritize quality time as their love language yearn for meaningful
+          and uninterrupted moments together. Engaging in shared activities,
+          having deep conversations, or simply enjoying each other's presence
+          without distractions are powerful ways to connect with them. Investing
+          time and energy into building shared experiences is crucial for
+          nurturing their love tank.
         </Paragraph>
-        <SubTitle>5. Massage the Base of the Ear</SubTitle>
+        <SubTitle>5. Physical Touch</SubTitle>
         <Paragraph>
-          After applying the cleaning solution, gently massage the base of the
-          ear for about 30 seconds. This helps to distribute the solution
-          throughout the ear canal and loosen any debris or wax buildup.
+          Physical touch plays a significant role in expressing love for
+          individuals whose primary love language is physical touch. Holding
+          hands, hugging, kissing, cuddling, or even a gentle touch on the arm
+          can convey love, comfort, and intimacy. These individuals often crave
+          physical closeness and thrive on the power of touch to feel deeply
+          connected and cherished.
         </Paragraph>
-        <SubTitle>6. Wipe the Ear Clean</SubTitle>
+
         <Paragraph>
-          Use a fresh cotton ball or gauze pad to wipe away any remaining
-          cleaning solution and debris from the ear. Be gentle and do not insert
-          anything into the ear canal.
-        </Paragraph>
-        <SubTitle>7. Reward Your Dog</SubTitle>
-        <Paragraph>
-          After cleaning your dog's ears, give them a treat and plenty of praise
-          for being a good boy or girl.
-        </Paragraph>
-        <SubTitle>8. Repeat as Needed</SubTitle>
-        <Paragraph>
-          You should clean your dog's ears once a week or as recommended by your
-          veterinarian. If your dog is prone to ear infections or has a lot of
-          hair in their ears, you may need to clean them more frequently.
-        </Paragraph>
-        <Paragraph>
-          In conclusion, cleaning your dog's ears is an essential part of their
-          grooming routine. With the right supplies and technique, you can help
-          prevent ear infections and keep your furry friend healthy and happy.
-          Remember to be gentle and take your time, and always reward your dog
-          for their cooperation. If you have any concerns about your dog's ear
-          health, consult with your veterinarian.
+          Recognizing and understanding the five love languages can
+          revolutionize our relationships. By identifying our own primary love
+          language and learning the love language of our partner, we can bridge
+          emotional gaps, enhance communication, and foster deeper connections.
+          The key lies in expressing love in a way that resonates with our loved
+          ones, acknowledging that we all have unique needs and preferences when
+          it comes to feeling cherished and valued. So, let's embark on this
+          journey of discovery, speaking the language of love fluently and
+          nurturing our relationships with greater understanding, compassion,
+          and affection. By doing so, we can create harmonious and fulfilling
+          connections that stand the test of time.
         </Paragraph>
       </ArticleContainer>
     </PageContainer>
@@ -137,7 +130,7 @@ const ImageContainer = styled.div`
 `;
 
 const Image = styled.img`
-  width: 300px;
+  width: 400px;
   height: 500px;
   object-fit: cover;
   border-radius: 10px;
