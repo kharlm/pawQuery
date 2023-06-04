d4fdfd3ebcf77d042e4149a2fa6bbe5a2eab1b85
kharlm <kharlmccatty@yahoo.com>
2023-06-04 09:24:16 -0400

    revamped website with header, needs a bit more edit but looks great

--- src/Home.js
+++ src/Home.js
@@ -3,30 +3,15 @@ import { Link } from "react-router-dom";
 
 import "./HomePage.css"; // Import the CSS file for styling
 import articles from "./config/articles";
+import Cards from "./components/Cards";
+import HeroSection from "./components/HeroSection";
 
 const HomePage = () => {
   return (
-    <div className="home-page">
-      <h1 className="home-page-title">Welcome to AllQuery</h1>
-      <div className="article-tiles">
-        {articles.map(article => (
-          <Link
-            key={article.id}
-            to={article.link} // Update the link path with the corresponding route path
-            className="article-link"
-          >
-            <div className="article-tile">
-              <img
-                src={article.image}
-                alt={article.caption}
-                className="article-image"
-              />
-              <div className="article-caption">{article.caption}</div>
-            </div>
-          </Link>
-        ))}
-      </div>
-    </div>
+    <>
+      <HeroSection />
+      <Cards />
+    </>
   );
 };
 
---
+++ src/components/Cards.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./Cards.css";
+import articles from "../config/articles";
+import CardItem from "./CardItem";
+
+function Cards() {
+  return (
+    <div className="cards">
+      <h1>Check out these articles! More to come</h1>
+      <div className="cards__container">
+        <div className="cards__wrapper">
+          <ul className="cards__items">
+            <CardItem
+              src="https://images.unsplash.com/photo-1516589178581-6cd7833ae3b2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bG92ZSUyMGxhbmd1YWdlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
+              text="What are the 5 Love Languages"
+              label=""
+              path="/lovelanguage"
+            />
+            <CardItem
+              src="https://images.unsplash.com/photo-1526925712774-2833a7ecd0d4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1574&q=80"
+              text="How to Screenshot on a Mac"
+              label=""
+              path="/screenshotmac"
+            />
+          </ul>
+          <ul className="cards__items">
+            <CardItem
+              src="https://images.unsplash.com/photo-1572525557337-8d520106d8fe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+              text="Who Escaped from Alcatraz"
+              label=""
+              path="/alcatrazescape"
+            />
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Cards;
